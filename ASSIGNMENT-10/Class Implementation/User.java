d9bed685766a7fc39e3f02486eccb71a92afab29
NtokozoPaul <139222130+NtokozoPaul@users.noreply.github.com>
2025-04-20 14:34:10 +0200

    Create User.java

---
+++ ASSIGNMENT-10/Class Implementation/User.java
@@ -0,0 +1,15 @@
+package com.elearning.model;
+
+public class User {
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
